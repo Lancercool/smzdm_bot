7e932bfb6bdb659b2d3e3e60528016a150b3ba72
Lancercool <42920935+Lancercool@users.noreply.github.com>
2021-11-10 19:36:48 +0800

    Update config.py

--- config.py
+++ config.py
@@ -18,7 +18,7 @@ DEFAULT_HEADERS = {
 """
 调试用 COOKIE
 """
-TEST_COOKIE = ''
+TEST_COOKIE = '__ckguid=qGI29Kl3i2HvytSKTnJqFQ5; __jsluid_s=7aa1b13fa5336188b3ad719b8dacc105; device_id=37231747921636285833866873a34d5b57a072010f1b7ac43bd1dc32d1; _gid=GA1.2.927592580.1636543364; homepage_sug=b; r_sort_type=score; footer_floating_layer=0; ad_date=10; ad_json_feed={}; _zdmA.vid=*; zdm_qd={"referrer":"https://github.com/Lancercool/smzdm_bot"}; _zdmA.time=1636543831721.6631.https://www.smzdm.com/; _zdmA.vid=*; _gat_UA-27058866-1=1; sensorsdata2015jssdkcross={"distinct_id":"17cfa3c61748d6-0d6c3f92743b8b-57b1a33-3686400-17cfa3c6175cda","first_id":"","props":{"$latest_traffic_source_type":"引荐流量","$latest_search_keyword":"未取到值","$latest_referrer":"https://github.com/Lancercool/smzdm_bot"},"$device_id":"17cfa3c61748d6-0d6c3f92743b8b-57b1a33-3686400-17cfa3c6175cda"}; sess=AT-yGWwZLhE3WHy9+Wa4U7kUhwdrWnM3mpX0ddqfb7mCHhOuj+9+kp7BcgnHUVoH/RDL8o+Pdkm3AUixKWof+0HpTxEGHhCTnkzT4Yf8BLs8P7ENIOFWBJGAa8d; user=user:6937191542|6937191542; smzdm_id=6937191542; _ga_09SRZM2FDD=GS1.1.1636543364.2.1.1636544098.0; _zdmA.uid=ZDMA.qaOxZraTf.1636544099.2419200; userId=user:6937191542|6937191542; _ga=GA1.2.1448650621.1636285835; bannerCounter=[{"number":0,"surplus":1},{"number":2,"surplus":1},{"number":2,"surplus":1},{"number":2,"surplus":1},{"number":2,"surplus":1}]; _zdmA.time=1636544115319.0.https://www.smzdm.com/'
 
 
 """
